e25e0d92ac82238a54023baa4559fb6df2b79ff5
Lucas Deijkers (LJM) <AO633249@achmea.nl>
2025-11-15 13:06:43 +0100

    extracted stock price

--- stock-picker/collect_data.py
+++ stock-picker/collect_data.py
@@ -7,14 +7,27 @@ API_KEY =  'sBRGxGtAXIFewvu3Sn6ePUoOC7RMFsvO3ArAWsuu'
 
 def get_stock_quote(ticker):
     url = f'{BASE_URL}/quote?symbols={ticker}&api_token={API_KEY}'
+
     response = requests.get(url)
-    print(url)
-    print('\n')
-    print(response.status_code)
+
+    if response.status_code == 200:
+        stock_data = response.json()
+        return stock_data
+
+    else:
+        print(f'Failed to retrieve data: {response.status_code}')
+
 
 stock = 'AAPL'
 
-get_stock_quote(stock)
+stock_response = get_stock_quote(stock)
+stock_data = stock_response['data']
+stock_data = stock_data[0]
+
+if stock_response:
+    print(f'{stock_data["ticker"]}: {stock_data["price"]}')
+
+
 
 
 
